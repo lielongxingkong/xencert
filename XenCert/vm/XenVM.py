17d54f2b7aa19e27e20d8ce551c54a8cf9a985aa
Zhao Zhenlong <zhaozhl@inspur.com>
2015-02-09 14:18:04 +0800

    add XenVM Class

---
+++ XenCert/vm/XenVM.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#
+# This program is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU Lesser General Public License as published 
+# by the Free Software Foundation; version 2.1 only.
+#
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# GNU Lesser General Public License for more details.
+#
+#
+
+import os, sys, re, time
+
+class XenVM():
+    """Xen Virtual Machine Class"""
+    def __init__(self):
+        pass
+
+    def create(self):
+        pass
+            
+    def remove(self):
+        pass
+
+    def load(self):
+        pass
+
+    def store(self):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
