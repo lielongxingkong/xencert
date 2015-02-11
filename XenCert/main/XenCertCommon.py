a839c8324777ce4b062a21b4c5fe0d419d2a36c0
Zhao Zhenlong <zhaozhl@inspur.com>
2015-02-11 12:34:00 +0800

    add after argument for vm test

--- XenCert/main/XenCertCommon.py
+++ XenCert/main/XenCertCommon.py
@@ -55,6 +55,7 @@ __vm_args__ = [
     ["name",          "name for virtual machine", " : ", None,        "optional", "-N", ""   ],
     ["rootDisk",      "root virtual disk for virtual machine", " : ", None,        "required", "-R", ""   ],
     ["storeOn",       "disk to create new data storage", " : ", None,        "optional", "-o", ""   ] ]
+    ["after",       "action after vm test ", " : ", None,        "optional", "-A", ""   ] ]
 
 __common__ = [    
     ["functional", "perform functional tests",                          " : ", None, "optional", "-f", ""],
